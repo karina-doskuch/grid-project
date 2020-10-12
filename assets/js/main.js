f7da7be0971e0f8f14b246c67d96df4b04cf20eb
Karina Doskuch <karina.doskuch@gmail.com>
2020-10-12 20:25:19 +0300

    Polishing styles.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,8 @@
-
 $(document).ready(function () {
 
-  $('.header-navbar__hamburger').on('click', function () {
-    $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
+  $('.header-navbar_btn').on('click', function () {
+    $(this).toggleClass('header-navbar_btn--active');
+    $('.header-navbar_list').stop(true, true).fadeToggle(500);
   });
 
 });
